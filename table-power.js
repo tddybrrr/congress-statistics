c6a2c7eada4161e114d83d67a1e6de9babdf27e7
teddy <theodorelrose@gmail.com>
2018-11-14 15:31:37 +0100

    house and senate data now work with filtering and live data

--- table-power.js
+++ table-power.js
@@ -1,211 +1,267 @@
- var tableDaddy = document.getElementById("tableZone");
- var table = document.createElement("table");
- tableDaddy.appendChild(table);
 
- function initialTable() {
-     for (i = 0; i < data.results[0].num_results; i++) {
+if (document.getElementById("senate") == null){
+    
+    fetch('https://api.propublica.org/congress/v1/115/house/members.json', {
+        headers: new Headers({
+            "X-API-Key": "VoUTxAXwkKdNuARhtuxtZnGwGaFZslYOMHD32Nw0"
+        })
+    })
+    .then(function (response) {
 
-         var row = table.insertRow(i)
+        return response.json()
+    })
+    .then(function (data) {
+        activateListeners(data)
+        printPosts(data)
+        initialTable(data)
+        generateHeader()
+        generateStateList()
+    })
 
-
-
-         var x = 0;
-
-         while (x < 4) {
-
-             var a = document.createElement('a')
-             var linkText = document.createTextNode(data.results[0].members[i].first_name + " " + replacer(data.results[0].members[i].middle_name) + " " + data.results[0].members[i].last_name)
-             a.appendChild(linkText)
-             a.title = "senator's personal website";
-             a.href = data.results[0].members[i].url
-
-             var column1 = row.insertCell(x);
-             column1.appendChild(a)
-             x++
-
-             var column2 = row.insertCell(x);
-             column2.innerHTML = (data.results[0].members[i].party)
-
-             x++
-
-             var column3 = row.insertCell(x);
-             column3.innerHTML = (data.results[0].members[i].state)
-             x++
-
-             var column4 = row.insertCell(x);
-             column4.innerHTML = (data.results[0].members[i].seniority)
-             x++
-
-             var column5 = row.insertCell(x);
-             column5.innerHTML = (data.results[0].members[i].votes_with_party_pct + "%")
-
-         }
+    
+} else{
+    fetch('https://api.propublica.org/congress/v1/115/senate/members.json', {
+        headers: new Headers({
+            "X-API-Key": "VoUTxAXwkKdNuARhtuxtZnGwGaFZslYOMHD32Nw0"
+        })
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        activateListeners(data)
+        printPosts(data)
+        initialTable(data)
+        generateHeader()
+        generateStateList()
+    })
+}
 
 
-     }
- }
- initialTable()
- generateHeader()
+function activateListeners(data) {
+    document.querySelector("#R").addEventListener('change', function(){
+                                myFunction(data)                  
+                                                  })
+    document.querySelector("#D").addEventListener('change', function(){
+                                myFunction(data)                  
+                                                  })
+    document.querySelector("#I").addEventListener('change', function(){
+                                myFunction(data)                  
+                                                  })
+    document.querySelector("#states").addEventListener('change', function(){
+                                myFunction(data)                  
+                                                  })
+}
 
- function myFunction() {
+function printPosts(someShit) {
+    console.log(someShit.results[0].members[2])
+}
 
-     var selectedParties = Array.from(document.querySelectorAll('input[name=politcal_party]:checked')).map(elt => elt.value)
 
-     var selectedState = document.querySelector('#states').value
+var tableDaddy = document.getElementById("tableZone");
+var table = document.createElement("table");
+tableDaddy.appendChild(table);
 
-     if ((selectedState == "returnAll") && (selectedParties.length == 3)) {
-         table.innerHTML = ""
-         initialTable()
-         generateHeader()
-         return;
-     } else if ((selectedState == "returnAll") && (selectedParties.length == 2 || selectedParties.length == 1)) {
+function initialTable(data) {
+    for (i = 0; i < data.results[0].num_results; i++) {
 
-         table.innerHTML = ""
+        var row = table.insertRow(i)
 
-         for (i = 0; i < data.results[0].num_results; i++) {
+        var x = 0;
 
-             var row = table.insertRow(i)
+        while (x < 4) {
 
-             if (selectedParties.indexOf(data.results[0].members[i].party) !== -1) {
+            var a = document.createElement('a')
+            var linkText = document.createTextNode(data.results[0].members[i].first_name + " " + replacer(data.results[0].members[i].middle_name) + " " + data.results[0].members[i].last_name)
+            a.appendChild(linkText)
+            a.title = "senator's personal website";
+            a.href = data.results[0].members[i].url
 
-                 var x = 0;
+            var column1 = row.insertCell(x);
+            column1.appendChild(a)
+            x++
 
-                 while (x < 4) {
+            var column2 = row.insertCell(x);
+            column2.innerHTML = (data.results[0].members[i].party)
 
-                     var a = document.createElement('a')
-                     var linkText = document.createTextNode(data.results[0].members[i].first_name + " " + replacer(data.results[0].members[i].middle_name) + " " + data.results[0].members[i].last_name)
-                     a.appendChild(linkText)
-                     a.title = "senator's personal website";
-                     a.href = data.results[0].members[i].url
+            x++
 
-                     var column1 = row.insertCell(x);
-                     column1.appendChild(a)
-                     x++
+            var column3 = row.insertCell(x);
+            column3.innerHTML = (data.results[0].members[i].state)
+            x++
 
-                     var column2 = row.insertCell(x);
-                     column2.innerHTML = (data.results[0].members[i].party)
-                     x++
+            var column4 = row.insertCell(x);
+            column4.innerHTML = (data.results[0].members[i].seniority)
+            x++
 
-                     var column3 = row.insertCell(x);
-                     column3.innerHTML = (data.results[0].members[i].state)
-                     x++
+            var column5 = row.insertCell(x);
+            column5.innerHTML = (data.results[0].members[i].votes_with_party_pct + "%")
 
-                     var column4 = row.insertCell(x);
-                     column4.innerHTML = (data.results[0].members[i].seniority)
-                     x++
+        }
 
-                     var column5 = row.insertCell(x);
-                     column5.innerHTML = (data.results[0].members[i].votes_with_party_pct + "%")
 
-                 }
+    }
+}
 
-             }
 
-         }
-         generateHeader()
-         return;
-     } else {
-         table.innerHTML = ""
-         for (i = 0; i < data.results[0].num_results; i++) {
+function myFunction(data) {
 
-             var row = table.insertRow(i)
-             if ((selectedParties.indexOf(data.results[0].members[i].party) !== -1) && (selectedState.indexOf(data.results[0].members[i].state) !== -1)) {
+    var selectedParties = Array.from(document.querySelectorAll('input[name=politcal_party]:checked')).map(elt => elt.value)
 
-                 var x = 0;
+    var selectedState = document.querySelector('#states').value
 
-                 while (x < 4) {
+    if ((selectedState == "returnAll") && (selectedParties.length == 3)) {
+        table.innerHTML = ""
+        initialTable(data)
+        generateHeader()
+        return;
+    } else if ((selectedState == "returnAll") && (selectedParties.length == 2 || selectedParties.length == 1)) {
 
-                     var a = document.createElement('a')
-                     var linkText = document.createTextNode(data.results[0].members[i].first_name + " " + replacer(data.results[0].members[i].middle_name) + " " + data.results[0].members[i].last_name)
-                     a.appendChild(linkText)
-                     a.title = "senator's personal website";
-                     a.href = data.results[0].members[i].url
+        table.innerHTML = ""
 
-                     var column1 = row.insertCell(x);
-                     column1.appendChild(a)
-                     x++
+        for (i = 0; i < data.results[0].num_results; i++) {
 
-                     var column2 = row.insertCell(x);
-                     column2.innerHTML = (data.results[0].members[i].party)
-                     x++
+            var row = table.insertRow(i)
 
-                     var column3 = row.insertCell(x);
-                     column3.innerHTML = (data.results[0].members[i].state)
-                     x++
+            if (selectedParties.indexOf(data.results[0].members[i].party) !== -1) {
 
-                     var column4 = row.insertCell(x);
-                     column4.innerHTML = (data.results[0].members[i].seniority)
-                     x++
+                var x = 0;
 
-                     var column5 = row.insertCell(x);
-                     column5.innerHTML = (data.results[0].members[i].votes_with_party_pct + "%")
+                while (x < 4) {
 
-                 }
-             }
+                    var a = document.createElement('a')
+                    var linkText = document.createTextNode(data.results[0].members[i].first_name + " " + replacer(data.results[0].members[i].middle_name) + " " + data.results[0].members[i].last_name)
+                    a.appendChild(linkText)
+                    a.title = "senator's personal website";
+                    a.href = data.results[0].members[i].url
 
-         }
+                    var column1 = row.insertCell(x);
+                    column1.appendChild(a)
+                    x++
 
-     }
+                    var column2 = row.insertCell(x);
+                    column2.innerHTML = (data.results[0].members[i].party)
+                    x++
 
-     generateHeader()
+                    var column3 = row.insertCell(x);
+                    column3.innerHTML = (data.results[0].members[i].state)
+                    x++
 
- }
+                    var column4 = row.insertCell(x);
+                    column4.innerHTML = (data.results[0].members[i].seniority)
+                    x++
 
- function headerNames(value) {
-     if (value == 0) {
-         return "full name"
-     } else if (value == 1) {
-         return "politcal party"
-     } else if (value == 2) {
-         return "state"
-     } else if (value == 3) {
-         return "senitory (years)"
-     } else if (value == 4) {
-         return "votes within party"
-     }
- }
+                    var column5 = row.insertCell(x);
+                    column5.innerHTML = (data.results[0].members[i].votes_with_party_pct + "%")
 
- function generateHeader() {
-     var header = table.createTHead();
-     var rowHead = header.insertRow(0);
-     for (y = 0; y < 5; y++) {
-         var cellular = rowHead.insertCell(y);
-         cellular.innerHTML = headerNames(y);
-     }
- }
+                }
 
- function generateStateList() {
-     var selectOption = document.createElement("option");
-     selectOption.text = "select";
-     selectOption.value = "returnAll";
-     var select = document.getElementById("states");
-     select.appendChild(selectOption);
-     for (var i = 0; i < usStates.length; i++) {
-         var option = document.createElement("option");
-         option.text = usStates[i].name;
-         option.value = usStates[i].abbreviation;
-         var select = document.getElementById("states");
-         select.appendChild(option);
-     }
- }
+            }
 
- function replacer(value) {
-     if (value === null) {
-         return " ";
-     } else {
-         return value;
-     }
- }
+        }
+        generateHeader()
+        return;
+    } else {
+        table.innerHTML = ""
+        for (i = 0; i < data.results[0].num_results; i++) {
 
- generateStateList()
+            var row = table.insertRow(i)
+            if ((selectedParties.indexOf(data.results[0].members[i].party) !== -1) && (selectedState.indexOf(data.results[0].members[i].state) !== -1)) {
 
- function redify() {
-     for (i = 0; i < rows.length; i++) {
-         cells = rows[i].getElementsByTagName('td');
-         if (cells[1].innerHTML == 'R')
-             rows[i].className = "red";
+                var x = 0;
 
-         if (cells[1].innerHTML == 'I')
-             rows[i].className = "grey";
-     }
- }
+                while (x < 4) {
+
+                    var a = document.createElement('a')
+                    var linkText = document.createTextNode(data.results[0].members[i].first_name + " " + replacer(data.results[0].members[i].middle_name) + " " + data.results[0].members[i].last_name)
+                    a.appendChild(linkText)
+                    a.title = "senator's personal website";
+                    a.href = data.results[0].members[i].url
+
+                    var column1 = row.insertCell(x);
+                    column1.appendChild(a)
+                    x++
+
+                    var column2 = row.insertCell(x);
+                    column2.innerHTML = (data.results[0].members[i].party)
+                    x++
+
+                    var column3 = row.insertCell(x);
+                    column3.innerHTML = (data.results[0].members[i].state)
+                    x++
+
+                    var column4 = row.insertCell(x);
+                    column4.innerHTML = (data.results[0].members[i].seniority)
+                    x++
+
+                    var column5 = row.insertCell(x);
+                    column5.innerHTML = (data.results[0].members[i].votes_with_party_pct + "%")
+
+                }
+            }
+
+        }
+
+    }
+
+    generateHeader()
+
+}
+
+function headerNames(value) {
+    if (value == 0) {
+        return "full name"
+    } else if (value == 1) {
+        return "politcal party"
+    } else if (value == 2) {
+        return "state"
+    } else if (value == 3) {
+        return "senitory (years)"
+    } else if (value == 4) {
+        return "votes within party"
+    }
+}
+
+function generateHeader() {
+    var header = table.createTHead();
+    var rowHead = header.insertRow(0);
+    for (y = 0; y < 5; y++) {
+        var cellular = rowHead.insertCell(y);
+        cellular.innerHTML = headerNames(y);
+    }
+}
+
+function generateStateList() {
+    var selectOption = document.createElement("option");
+    selectOption.text = "select";
+    selectOption.value = "returnAll";
+    var select = document.getElementById("states");
+    select.appendChild(selectOption);
+    for (var i = 0; i < usStates.length; i++) {
+        var option = document.createElement("option");
+        option.text = usStates[i].name;
+        option.value = usStates[i].abbreviation;
+        var select = document.getElementById("states");
+        select.appendChild(option);
+    }
+}
+
+function replacer(value) {
+    if (value === null) {
+        return " ";
+    } else {
+        return value;
+    }
+}
+
+
+function redify() {
+    for (i = 0; i < rows.length; i++) {
+        cells = rows[i].getElementsByTagName('td');
+        if (cells[1].innerHTML == 'R')
+            rows[i].className = "red";
+
+        if (cells[1].innerHTML == 'I')
+            rows[i].className = "grey";
+    }
+}
